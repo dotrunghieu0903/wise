c3652d90201134517af6eb9bcd5b1334ad762cc9
phamthibichnga <20521642@gm.uit.edu.vn>
2025-08-15 16:22:09 +0700

    Updated run of wikimedia-commons-25

--- run.sh
+++ run.sh
@@ -82,8 +82,13 @@ python extract-features.py wise-data/wikimedia-commons-25 --project-dir wise-pro
 python media-metadata.py import --metadata-id "wikimedia-commons-25" --from-csv wise-data/wikimedia-commons-25/media-metadata.csv --metadata-type "media" --project-dir wise-projects/Wikimedia-Commons-25
 
 # create index in database
-# python create-index.py --project-dir wise-projects/Wikimedia-Commons-25/ --fts-config wise-projects/Wikimedia-Commons-25/fts_config.json
+python create-index.py --project-dir wise-projects/Wikimedia-Commons-25/ --fts-config wise-projects/Wikimedia-Commons-25/fts_config.json
 
+# Test server to directly search
+python serve.py --project-dir wise-projects/Wikimedia-Commons-25/
+
+# Exhaustive
 python search.py --queries-from wise-data/wikimedia-commons-25/sample_queries.csv --in video --topk 100 --index-type IndexFlatIP --result-format csv --save-to-file wise-test/exhaustive-wikimedia-commons-25.csv --project-dir wise-projects/Wikimedia-Commons-25
 
+# ANN
 python search.py --queries-from wise-data/wikimedia-commons-25/sample_queries.csv --in video --topk 100 --index-type IndexIVFFlat --result-format csv --save-to-file wise-test/ann-wikimedia-commons-25.csv --project-dir wise-projects/Wikimedia-Commons-25
